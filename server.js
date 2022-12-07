727c0dfa4daef50e41501b7c084316d157470777
sagar gohel <sagar.180180107011@gmail.com>
2022-12-07 11:16:42 +0530

    initial express setup

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const http = require("http");
 
 //Load env variables
 dotenv.config({ path: "./config/config.env" });
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.write("Hello, world!");
-    res.end("done!")
-})
 const app = express();
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`));
