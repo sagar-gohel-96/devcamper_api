6aa8b7cf6cf8e1606dfef529b2585a4d31e7a8d6
sagar gohel <sagar.180180107011@gmail.com>
2022-12-07 17:44:26 +0530

    middleware

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const http = require("http");
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
 //Route file
-const bootcamp = require("./routes/bootcamp");
+const bootcamp = require('./routes/bootcamp');
 //Load env variables
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 const app = express();
+
+// app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 //Mount routers
-app.use("/api/v1/bootcamps",bootcamp)
+app.use('/api/v1/bootcamps/', bootcamp);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`)
-)
+);
