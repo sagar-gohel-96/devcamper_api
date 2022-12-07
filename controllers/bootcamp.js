e747d18f3b6250abf4b4e8968bf89474ae2d82a3
sagar gohel <sagar.180180107011@gmail.com>
2022-12-07 15:46:14 +0530

    controllring set

---
+++ controllers/bootcamp.js
@@ -0,0 +1,39 @@
+// @description Get all bootcamps
+// @route  GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ sucess: true, msg: "Show all bootcamps" });
+};
+
+// @description Get single bootcamps
+// @route  GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ sucess: true, msg: "Show  bootcamp" });
+};
+
+// @description Create new bootcamp
+// @route  POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ sucess: true, msg: "Create new bootcamp" });
+};
+
+// @description Update bootcamp
+// @route  PUT /api/v1/bootcamp/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, msg: `Upadte bootcamp ${req.params.id} ` });
+};
+
+// @description Delete bootcamp
+// @route  PUT /api/v1/bootcamp/:id
+// @access  Private
+
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id} ` });
+};
--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.status(200).json({ sucess: true, msg: "Show all bootcamps" });
-});
-router.post("/", (req, res) => {
-  res.status(200).json({ sucess: true, msg: "Create new bootcamp" });
-});
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Upadte bootcamp ${req.params.id} ` });
-});
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id} ` });
-});
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  deleteBootcamp,
+  updateBootcamp,
+} = require("../controllers/bootcamp");
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 module.exports = router;
